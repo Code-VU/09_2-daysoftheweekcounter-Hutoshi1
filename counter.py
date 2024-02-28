911f8908eef4e5bd9d2c8b8b06c78d3962d4daa7
Tony Davis <Tony.Davis@vu.com>
2024-02-27 18:11:44 -0600

    add loop and update dictionary

--- counter.py
+++ counter.py
@@ -1,8 +1,20 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    fhand = open(file_name)
+    commitDay= dict()
 
-
+    for line in fhand:
+        if not line.startswith("From "): continue
+        line = line.split()
+        # print(line)
+        day = line[2]
+        if day not in commitDay:
+            commitDay[day] = 1
+        else:
+            commitDay[day] += 1
+    
+    print(commitDay)
 
 ## if you want to test locally run > python payCalculator.py
 if __name__ == "__main__":
